95d7c44981ec752c4701bd773662cfa2763fdad1
Myra Rozmiarek <11marozmi@gmail.com>
2020-09-28 09:35:55 -0500

    added html text, and JS

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var startQuizBtn = document 
-var questions = document
-var scores
-var timer 
+var startQuizBtn = document.getElementById("start");
+var questions = document.getElementById("questions");
+var scores = document.getElementById("scores");
+var timer = document.getElementById("timer");
+var answers = document.getElementById("answers");
+var mainDisplay = document.getElementById("mainDisplay")
+
+var quizIndex = 0;
+var timeLeft = 75;
+
+var quiz = [
+   question1 = {
+       questions: "here is my first question?",
+       answers: "answer 1",
+       answers: "answer 2",
+       answers: "answer 3",
+       answers: "ansdwer 4",       
+   }
+]
+
+function displayQuestion(index) {
+    mainDisplay.textContent = quiz[index].questions.toString();
+};
